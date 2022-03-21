f4b273e39c30293a83b1d0840288e45357003850
rewyr <rewyr@LAPTOP-QUEA6UE5>
2022-03-21 12:51:28 -0700

    Add project files.

---
+++ ADN102.04.Methods/Page09.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Page9
+{
+    // See if you can correct the compiler errors, and get the code to compile.
+
+    // instructions:  uncomment lines 11-12 and line 17.
+    // correct all the errors and run the program
+    public class StudentProgram
+    {
+        public static void StudentMain()
+        {
+            //int x = Hypotenuse(10.0, 20.0);
+            //Console.WriteLine(x);
+        }
+
+        static double hypotenues(double a, double b)
+        {
+            //return Math.Sqrt(a * a - b * b);
+            return 0;
+
+        }
+    }
+    
+}
